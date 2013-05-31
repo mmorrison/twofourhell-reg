d4a0a7899b5b44fb982a2fa2ec4df58685e33018
Ben Haan <benhaan@gmail.com>
2013-05-31 00:56:19 -0500

    fix root in routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ TwofourhellVolReg::Application.routes.draw do
 
   resources :volunteers
 
-  root 'signup/new'
+  root 'signup#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

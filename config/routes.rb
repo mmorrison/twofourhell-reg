cf7b1b72e0d884af53fd1843797af1b622c03ec0
Ben Haan <benhaan@gmail.com>
2013-05-31 00:39:34 -0500

    set root and added unicorn config

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ TwofourhellVolReg::Application.routes.draw do
 
   resources :volunteers
 
+  root 'signup/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
